6fea5533ba91e8a166342eebdd4811cf4c0241d3
eunji9677 <eunji71234@gmail.com>
2023-03-06 17:24:28 +0900

    second commit

---
+++ front/src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <!-- <router-link to="/about">About</router-link> | -->
+        <router-link to="/board/list">board  </router-link> |
+        <router-link to="/attitudeBook/list">attitude book list</router-link>
+      </div>
+    </header>
+
+</template>
+  
+<script>
+  export default {
+  
+  }
+</script>
+  
+<style scoped>
+  
+</style>
---
+++ front/src/views/PageHome.vue
@@ -0,0 +1,17 @@
+<template> 
+    <div class="home">
+         <img alt="Vue logo" src="../assets/logo.png"> 
+         <HelloWorld msg="Welcome to Your Vue.js App"/> 
+    </div> 
+    </template>
+    
+    <script> 
+    // @ is an alias to /src 
+    import HelloWorld from '@/views/page/login.vue' 
+    export default { 
+        name: 'PageHome',
+         components: { 
+            HelloWorld 
+        }
+         }
+     </script>
