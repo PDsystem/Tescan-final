6e98c520182810dc00a4a0cab4b14c10ceef2fb8
youngcheon <young@example.com>
2023-03-06 17:17:42 +0900

    Revert "first commit"
    This reverts commit a6e03e7c40f0ca8fbe5844b1cb16c6e7c5b043cc.

--- back/src/main/java/com/example/springboot/back/Login/entity/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.back.Login.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login,String> {
-    Login findByContentIdAndContentPw(String contentId,String contentPw);
-}
-
