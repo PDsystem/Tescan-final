35cce1c0233fe3068e19b7fb39a9653e65d3fbf2
joguri33 <oou0803@naver.com>
2023-03-15 17:25:28 +0900

    0315 조진형 header 1차 수정

--- front/src/components/PageHeader.vue
+++ front/src/components/PageHeader.vue
@@ -2,51 +2,43 @@
   <header>
     <!-- Homebtn -->
     <div class="homeBtn"><router-link to="/"><img alt="Tescan logo" src="../assets/tescanK_menu.jpg"></router-link></div>
-    <nav_wrap>
-      <div class="nav">
-        <!-- Navbar -->
-        <nav>
-          <ui class="navbar">
-            <li>
-              |<router-link to="/">Home</router-link> |
+    <!-- navBar -->
+    <div class="navbar-body">
+      <div class="container cn_headerline">
+        <div class="collapse navbar-collapse navbar-responsive-collapse">
+          <ui id="nav">
+            <li><a href="/">Home</a>
               <ul>
                 <li><router-link to="/board/list">board</router-link></li>
                 <li><router-link to="/attitudeBook/list">attitude book list</router-link></li>
               </ul>
             </li>
-            <li>
-              |<a href="/">공통기능</a>|
+            <li><a href="/">공통기능</a>
               <ul>
                 <li><a href="/">공통코드</a></li>
                 <li><a href="/">공통코드상세</a></li>
               </ul>
             </li>
-            <li>
-              |<a href="/emp">내부관리</a>|
+            <li><a href="/emp">내부관리</a>
               <ul>
                 <li><a href="/emp">사원등록</a></li>
                 <li><a href="/">장비관리</a></li>
               </ul>
             </li>
-            <li>
-              |<a href="/customer/list">거래처 관리</a>|
+            <li><a href="/customer/list">거래처 관리</a>
               <ul>
                 <li><a href="/customer/write">거래처 등록</a></li>
                 <li><a href="/customer/list">거래처 현황</a></li>
-<!--                <li><a href="/client/list">거래처등록</a></li>
-                <li><a href="/customer/list">거래처현황</a></li> -->
               </ul>
             </li>
-            <li>
-              |<a href="/salesOppo/oppo">영업부관리</a>|
+            <li><a href="/salesOppo/oppo">영업부관리</a>
               <ul>
                 <li><a href="/salesOppo/oppo">영업기회</a></li>
                 <li><a href="/sales/list">영업방문</a></li>
                 <li><a href="">업무보고</a></li>
               </ul>
             </li>
-            <li>
-              |<a href="/">기술부관리</a>|
+            <li><a href="/">기술부관리</a>
               <ul>
                 <li><a href="/">업무내역관리</a></li>
                 <li><a href="/">거래처별 업무</a></li>
@@ -54,17 +46,17 @@
                 <li><a href="/">업무보고</a></li>
               </ul>
             </li>
-            <li>
-              |<a href="/askBoard/list">게시판</a>|
+            <li><a href="/askBoard/list">게시판</a>
               <ul>
                 <li><a href="/askBoard/list">질문게시판</a></li>
               </ul>
             </li>
           </ui>
-        </nav>
+        </div>
       </div>
-    </nav_wrap>
-  </header>  
+    </div> 
+  </header>
+  <br>
 </template>
 
 <script>
@@ -75,45 +67,72 @@ export default {
 
 <style>
 .homeBtn {
-  padding: 30px;
+  padding: 20px;
 }
 
-.nav_wrap {
-  width: 100%;
-  max-width: 660px;
-  float: none;
-  margin: auto;
-  z-index: 100;
+.navbar-body{
+  position: relative;
+  padding-left: 30px;
+
+}
+.container {
+  position: relative;
+  height: 60px;
+  padding-top: 30px;
+  background: #f0f0f0;
+}
+#nav {
+  list-style: none;
+  height: 20px;
+  background: #f0f0f0;
 }
 
-.navbar {
-  display: inline-table;
-  justify-content: space-around;
-}
-
-.navbar>li {
+#nav li {
   float: left;
+  position: relative;
+  margin: 0px 40px;
 }
 
-.navbar>li ul {
-  display: none;
-}
-
-.navbar>li:hover>ul {
+#nav li a {
   display: block;
+  padding: 0px 20px;
+  margin: 0px;
+  font-weight: 600;
+  font-size: large;
+  color: #0078b3;
+  text-decoration: none;
 }
 
-.navbar>li {
+#nav ul {
+  margin: 0px 10px;
+  padding: 0;
   list-style: none;
-  font-size: 15px;
-  font-weight: 900;
-  margin-block-start: 1em;
-  margin-block-end: 1em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  padding-inline-start: 40px;
+  position: absolute;
+  left: 0;
+  top: 55px;
+  width: 200px;
+  opacity: 0; /*네비바 비활성화*/
 }
 
-.navbar>li>ul {
-  list-style: none;
-}</style>
+#nav li:hover ul{
+  opacity: 1; /*네비바 활성화*/
+}
+
+#nav ul li {
+  float: center;
+  margin: 0px;
+  padding: 0px;
+}
+
+#nav ul li a {
+  font-weight: 500;
+  color: #0078b3;
+  background: #f0f0f0;
+  border: 2px solid #0078b3;
+}
+
+#nav ul li a:hover {
+  background: #0078b3;
+  color: #f0f0f0;
+}
+</style>
